266eb8a108a108189473856ddc87dfdcc3111bec
Tikhon Nikita <tikhon.nikita@gmail.com>
2025-10-30 19:39:57 +0300

    GTW-4: add Jwt filter

---
+++ gateway-service/src/main/java/com/innowise/gatewayservice/config/constant/SecurityConstant.java
@@ -0,0 +1,14 @@
+package com.innowise.gatewayservice.config.constant;
+
+public class SecurityConstant {
+
+    private SecurityConstant() {}
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_BEARER_PREFIX = "Bearer ";
+    public static final int AUTHORIZATION_TOKEN_POSITION = 7;
+
+    public static final String TOKEN_CLAIM_USER_ID = "userId";
+    public static final String TOKEN_CLAIM_ROLES = "roles";
+
+}
