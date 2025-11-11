c0cfd3b16c601a4fd40e6246bd2176738cd6a36c
Tikhon Nikita <tikhon.nikita@gmail.com>
2025-11-11 16:52:04 +0300

    PMT-4: add configuration for communication with external API to generate random number

---
+++ payment-service/src/main/java/com/innowise/paymentservice/config/ServiceConfig.java
@@ -0,0 +1,35 @@
+package com.innowise.paymentservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
+
+@Configuration
+public class ServiceConfig {
+
+    @Value("${services.random-number-api.uri}")
+    private String randomNumberApiUri;
+
+    @Bean
+    public RestClient randomNumberRestClient() {
+        return RestClient.builder()
+                .baseUrl(randomNumberApiUri)
+                .requestFactory(clientHttpRequestFactory())
+                .build();
+    }
+
+    @Bean
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(Duration.ofSeconds(5));
+        factory.setReadTimeout(Duration.ofSeconds(5));
+
+        return factory;
+    }
+
+}
