5f780df40871a93aaa0be42278b2c03a74db2c4e
Tikhon Nikita <tikhon.nikita@gmail.com>
2025-11-11 16:56:15 +0300

    PMT-4: add exceptions for external API

---
+++ payment-service/src/main/java/com/innowise/paymentservice/exception/ExternalServiceException.java
@@ -0,0 +1,14 @@
+package com.innowise.paymentservice.exception;
+
+import java.io.Serial;
+
+public class ExternalServiceException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1208058532126723309L;
+
+    public ExternalServiceException(String message) {
+        super(message);
+    }
+
+}
