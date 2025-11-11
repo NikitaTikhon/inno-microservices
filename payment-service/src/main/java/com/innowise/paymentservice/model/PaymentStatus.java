cac1fc7876b7e6b137c55ebea4b46b2e4e7b4fc9
Tikhon Nikita <tikhon.nikita@gmail.com>
2025-11-11 16:40:47 +0300

    PMT-3: add entities

---
+++ payment-service/src/main/java/com/innowise/paymentservice/model/PaymentStatus.java
@@ -0,0 +1,10 @@
+package com.innowise.paymentservice.model;
+
+public enum PaymentStatus {
+    PENDING,
+    PROCESSING,
+    SUCCESS,
+    FAILED,
+    REFUNDED,
+    CANCELLED
+}
---
+++ payment-service/src/main/java/com/innowise/paymentservice/model/document/Payment.java
@@ -0,0 +1,47 @@
+package com.innowise.paymentservice.model.document;
+
+import com.innowise.paymentservice.model.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "payments")
+public class Payment {
+
+    @Id
+    private ObjectId id;
+
+    @Indexed
+    @Field("order_id")
+    private Long orderId;
+
+    @Indexed
+    @Field("user_id")
+    private Long userId;
+
+    private PaymentStatus status;
+
+    @CreatedDate
+    private LocalDateTime timestamp;
+
+    @Field(value = "payment_amount", targetType = FieldType.DECIMAL128)
+    private BigDecimal paymentAmount;
+
+}
